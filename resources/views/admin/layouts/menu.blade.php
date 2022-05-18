8084b7645666a1f4d3a931429c7ea4c35d6b5e62
asrafulhaq <devvorb@gmail.com>
2022-05-18 20:53:07 +0600

    slice admin pages

---
+++ resources/views/admin/layouts/menu.blade.php
@@ -0,0 +1,27 @@
+<!-- Sidebar -->
+<div class="sidebar" id="sidebar">
+    <div class="sidebar-inner slimscroll">
+        <div id="sidebar-menu" class="sidebar-menu"> 
+            <ul>
+
+                <li class="active"> 
+                    <a href="index.html"><i class="fe fe-home"></i> <span>Dashboard</span></a>
+                </li>
+                <li> 
+                    <a href="appointment-list.html"><i class="fe fe-layout"></i> <span>Slider</span></a>
+                </li>
+
+                
+                <li class="submenu">
+                    <a href="#"><i class="fe fe-users"></i> <span> Admin User </span> <span class="menu-arrow"></span></a>
+                    <ul style="display: none;">
+                        <li><a href="error-404.html"> Admin User </a></li>
+                        <li><a href="error-500.html"> Role </a></li>
+                        <li><a href="error-500.html"> Permission </a></li>
+                    </ul>
+                </li>
+            </ul>
+        </div>
+    </div>
+</div>
+<!-- /Sidebar -->
